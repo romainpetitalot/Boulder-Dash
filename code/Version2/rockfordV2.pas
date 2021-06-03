db9e1a70e08a843a1b6bc6e8a4d2b3a5ae2c459a
PETITALOT ROMAIN <romain.petitalot@insa-rouen.fr>
2021-06-03 13:12:24 +0200

    Ajout chrono

--- code/Version2/rockfordV2.pas
+++ code/Version2/rockfordV2.pas
@@ -1,6 +1,6 @@
 program Rockfrd;
 
-uses SDL, sdl_image, sysutils,menurockford;
+uses SDL, sdl_image, sdl_ttf, sysutils,menurockford;
 
 CONST longueur = 24;
 		largueur = 20;
@@ -22,92 +22,42 @@ procedure initialise( var window, rockford : PSDL_Surface);
 begin
 	SDL_Init(SDL_INIT_VIDEO + SDL_INIT_AUDIO);
 	SDL_putenv('SDL_VIDEO_WINDOW_POS=center');
-	window := SDL_SetVideoMode(32*longueur, 32*largueur, 32, SDL_DOUBLEBUF + SDL_HWSURFACE + SDL_NOFRAME);
+	window := SDL_SetVideoMode(32*longueur, 32*largueur+50, 32, SDL_DOUBLEBUF + SDL_HWSURFACE + SDL_NOFRAME);
 	
 	rockford := IMG_Load('ressources/face.png');
 	
 	SDL_ShowCursor(SDL_DISABLE);
 end;
 
-
-procedure afficherfondDeplacement(var window : PSDL_Surface;var T : Terrain);
-var fond, terre, bordure, pierre, diamant, port, spider : PSDL_Surface;
-	coordfond : TSDL_Rect;
-	i, j : Integer;
+procedure ecrire(var window : PSDL_Surface; txt : String; x, y, taille, R, V, B : Integer);
+var police : pTTF_Font;
+	couleur : PSDL_Color;
+	texte : PSDL_Surface;
+	ptxt: pChar;
+	position : TSDL_Rect;
 begin
-	fond := IMG_Load('ressources/fond.png');
-	terre := IMG_Load('ressources/terre.png');
-	bordure := IMG_Load('ressources/bordure.png');
-	pierre := IMG_Load('ressources/pierre.png');
-	diamant := IMG_Load('ressources/Diamond1.png');
-	port := IMG_Load('ressources/Port1.png');
-	spider := IMG_Load('ressources/Spider2.png');
+	if TTF_INIT=-1 then HALT;
+	
+	police := TTF_OPENFONT('ressources/RWC.TTF',taille); //Police RWC
+	new(couleur);
+	couleur^.r:=R; couleur^.g:=V;    couleur^.b:=B; //Couleur qu'on prend en entrée
 
-	coordfond.x := 0;
-	coordfond.y := 0;
+	ptxt := StrAlloc(length(txt) + 1); 
+	StrPCopy(ptxt, txt);
+	texte := TTF_RenderText_Solid(police, ptxt, couleur^);
+	position.x := x; position.y := y;	
+	strDispose(ptxt);
 	
-	for i := 1 to largueur do
-	begin
-		for j := 1 to longueur do
-		begin
-			coordfond.x := (j-1)*32;
-			
-			case T[i][j].genre of
-			
-				1 : SDL_BlitSurface(terre, NIL, window,@coordfond);
-				
-				2 :	SDL_BlitSurface(bordure, NIL, window,@coordfond);				
-			
-				3 : SDL_BlitSurface(pierre, NIL, window,@coordfond);
-				
-				4 : SDL_BlitSurface(diamant, NIL, window,@coordfond);
-				
-				5 : SDL_BlitSurface(port, NIL, window,@coordfond);
-		
-				6 : SDL_BlitSurface(spider, NIL, window,@coordfond);
-		
-				0 : SDL_BlitSurface(fond, NIL, window,@coordfond);	
-			end;
-		end;
-		coordfond.y := (i)*32
-	end;
-	
-	for i := 1 to largueur do //Lorsqu'une Pierre ou un diamant est en train de tomber
-	begin
-		for j := 1 to longueur do
-		begin
-			coordfond.x := (j-1)*32;
-						
-			if T[i][j].mouvement = 'Pierre' then
-			begin
-				coordfond.y := coordfond.y + 16;
-				
-				SDL_BlitSurface(pierre, NIL, window,@coordfond);
-				coordfond.y := coordfond.y - 16;
-			end;
-						
-			if T[i][j].mouvement = 'Diamant' then
-			begin
-				coordfond.y := coordfond.y + 16;
-				
-				SDL_BlitSurface(diamant, NIL, window,@coordfond);
-				coordfond.y := coordfond.y - 16;
-			end;
-		end;
-		coordfond.y := (i)*32
-	end;
-	
-	SDl_Flip(window);
-	SDL_FreeSurface(fond);
-	SDL_FreeSurface(terre);
-	SDL_FreeSurface(bordure);
-	SDL_FreeSurface(diamant);
-	SDL_FreeSurface(port);
-	SDL_FreeSurface(pierre);
-	SDL_FreeSurface(spider);
+	SDL_BlitSurface(texte, NIL, window, @position);
+
+	DISPOSE(couleur);
+	TTF_CloseFont(police);
+    TTF_Quit();
+    SDL_FreeSurface(texte);
 end;
 
-procedure afficherfond(var window, rockford : PSDL_Surface; T : Terrain; position : coordonnees);
+
+procedure afficherfond(var window, rockford : PSDL_Surface; T : Terrain; position : coordonnees; enDeplacement : Boolean);
 var fond, terre, bordure, pierre, diamant, port, spider : PSDL_Surface;
 	coordfond : TSDL_Rect;
 	i, j : Integer;
@@ -121,7 +71,7 @@ begin
 	spider := IMG_Load('ressources/Spider2.png');
 
 	coordfond.x := 0;
-	coordfond.y := 0;
+	coordfond.y := 50;
 	
 	for i := 1 to largueur do
 	begin
@@ -145,9 +95,10 @@ begin
 				0 : SDL_BlitSurface(fond, NIL, window,@coordfond);	
 			end;	
 		end;
-		coordfond.y := (i)*32
+		coordfond.y := coordfond.y + 32
 	end;
 	
+	coordfond.y := 50;
 	for i := 1 to largueur do
 	begin
 		for j := 1 to longueur do
@@ -170,14 +121,16 @@ begin
 				coordfond.y := coordfond.y - 16;
 			end;
 		end;
-		coordfond.y := (i)*32
+		coordfond.y := coordfond.y + 32
 	end;
 	
-	coordfond.x := 32*(position.x-1);
-	coordfond.y := 32*(position.y-1);
-	
-	SDL_BlitSurface(rockford, NIL, window,@coordfond);
-	
+	if enDeplacement then
+	begin
+		coordfond.x := 32*(position.x-1);
+		coordfond.y := 32*(position.y-1) + 50;
+		
+		SDL_BlitSurface(rockford, NIL, window,@coordfond);
+	end;
 	SDl_Flip(window);
 	SDL_FreeSurface(fond);
 	SDL_FreeSurface(terre);
@@ -189,10 +142,11 @@ begin
 end;
 
 procedure deplacementRockFordHoriz(var window : PSDL_Surface; direction : String; var coordRF : TSDL_Rect; sens : Integer; var pos : Integer; var T : Terrain);
-var RF1, RF2, RF3 : PSDL_Surface;
+var RF1, RF2, RF3, SurfaceFake : PSDL_Surface;
 	chemin : String;
 	pimage : PChar;
 	i : Integer;
+	coordFake : coordonnees;
 begin
 	chemin := 'ressources/'+direction+'1.png';
 	pimage := StrAlloc(length(chemin)+1);
@@ -212,19 +166,19 @@ begin
 	for i := 1 to 1 do
 	begin
 		coordRF.x := coordRF.x + sens*8;
-		afficherfondDeplacement(window, T);
+		afficherfond(window, SurfaceFake, T, coordFake, False);
 		SDL_BlitSurface(RF1, NIL, window,@coordRF);
 		SDl_Flip(window);
 		SDl_Delay(20);
 		
 		coordRF.x := coordRF.x + sens*12;
-		afficherfondDeplacement(window, T);
+		afficherfond(window, SurfaceFake, T, coordFake, False);
 		SDL_BlitSurface(RF2, NIL, window,@coordRF);
 		SDl_Flip(window);
 		SDl_Delay(20);
 		
 		coordRF.x := coordRF.x + sens*12;
-		afficherfondDeplacement(window, T);
+		afficherfond(window, SurfaceFake, T, coordFake, False);
 		SDL_BlitSurface(RF3, NIL, window,@coordRF);
 		SDl_Flip(window);
 		SDl_Delay(20);
@@ -238,10 +192,11 @@ begin
 end;
 
 procedure deplacementRockFordVert(var window : PSDL_Surface; direction : String; var coordRF : TSDL_Rect; sens : Integer; var pos : Integer; var T : Terrain);
-var RF1, RF2, RF3, RF4 : PSDL_Surface;
+var RF1, RF2, RF3, RF4, SurfaceFake : PSDL_Surface;
 	chemin : String;
 	pimage : PChar;
 	i : Integer;
+	coordFake : coordonnees;
 begin
 	chemin := 'ressources/'+direction+'1.png';
 	pimage := StrAlloc(length(chemin)+1);
@@ -266,25 +221,25 @@ begin
 	for i := 1 to 1 do
 	begin
 		coordRF.y := coordRF.y + sens*8;
-		afficherfondDeplacement(window, T);
+		afficherfond(window, SurfaceFake, T, coordFake, False);
 		SDL_BlitSurface(RF1, NIL, window,@coordRF);
 		SDl_Flip(window);
 		SDl_Delay(15);
 		
 		coordRF.y := coordRF.y + sens*8;
-		afficherfondDeplacement(window, T);
+		afficherfond(window, SurfaceFake, T, coordFake, False);
 		SDL_BlitSurface(RF2, NIL, window,@coordRF);
 		SDl_Flip(window);
 		SDl_Delay(15);
 		
 		coordRF.y := coordRF.y + sens*8;
-		afficherfondDeplacement(window, T);
+		afficherfond(window, SurfaceFake, T, coordFake, False);
 		SDL_BlitSurface(RF3, NIL, window,@coordRF);
 		SDl_Flip(window);
 		SDl_Delay(15);
 		
 		coordRF.y := coordRF.y + sens*8;
-		afficherfondDeplacement(window, T);
+		afficherfond(window, SurfaceFake, T, coordFake, False);
 		SDL_BlitSurface(RF4, NIL, window,@coordRF);
 		SDl_Flip(window);
 		SDl_Delay(15);
@@ -317,7 +272,7 @@ begin
 			begin
 				T[i+1][j].genre := numeroObj;
 				T[i][j].mouvement := '';
-				afficherfond(window, rockford, T, position);
+				afficherfond(window, rockford, T, position, True);
 				if (position.y = i+2) and (position.x = j) then 
 					writeln('dead');
 				if T[i+2][j].genre = 6 then
@@ -341,7 +296,7 @@ begin
 				begin
 					T[i][j].genre := 0;
 					T[i][j].mouvement := nomObjet;
-					afficherfond(window, rockford, T, position);
+					afficherfond(window, rockford, T, position, True);
 					SDL_Flip(window);
 				end;
 			end;					
@@ -442,46 +397,18 @@ begin
 							T[i][j].orientation := 'haut'
 					end;
 				end;	
-				afficherfond(window, rockford, T, position);
+				afficherfond(window, rockford, T, position, True);
 				SDL_Flip(window);	
 			end;
 		end;
 	end;
 end;
 
-procedure deplacementRF(var window, rockford : Psdl_Surface; var T:Terrain; var position : coordonnees; var coord : TSDL_Rect;var fin, u, d, r, l : Boolean;var nbDiamant:Integer);
-var event : TSDL_event;
-	bloquer, portActive : Boolean;
-begin
-	SDL_PollEvent(@event);
-	bloquer := False;
-	portActive := False;
-	
-	if nbDiamant > 1 then
-	begin
-		T[19][19].genre := 5;
-		portActive := True
-	end;
-	
-	case event.type_ of
-		SDL_KEYDOWN : 
-			case event.key.keysym.sym of 
-				SDLK_escape : fin := True;
-				SDLK_UP : u := True;
-				SDLK_DOWN : d := True;
-				SDLK_right : r := True;
-				SDLK_left : l := True;
-		end;
 
-		SDL_KEYUP : 
-			case event.key.keysym.sym of 
-				SDLK_UP : u := False;
-				SDLK_DOWN : d := False;
-				SDLK_right : r := False;
-				SDLK_left : l := False;
-		end;
-	end;
-	
+procedure gestionDeplacement(var window : Psdl_Surface; var T:Terrain; u, d, r, l : Boolean; var position : coordonnees; var coord : TSDL_Rect; var nbDiamant:Integer);
+var bloquer : Boolean;
+begin
+	bloquer := False;
 	if u then 
 	begin
 		if T[position.y-1][position.x].genre = 1 then T[position.y-1][position.x].genre := 0; 
@@ -504,7 +431,6 @@ begin
 			nbDiamant := nbDiamant + 1
 		end;
 		
-	
 		if not(T[position.y+1][position.x].genre = 2) and not(T[position.y+1][position.x].genre = 3) then 
 			deplacementRockFordVert(window, 'bas', coord, 1, position.y, T);
 	end;
@@ -558,9 +484,45 @@ begin
 			else bloquer := True					
 		end;		
 		if not(T[position.y][position.x-1].genre = 2) and not(bloquer) then
-		deplacementRockFordhoriz(window, 'gauche', coord, -1, position.x, T);
+			deplacementRockFordhoriz(window, 'gauche', coord, -1, position.x, T);
+	end;
+end;
+
+
+procedure deplacementRF(var window, rockford : Psdl_Surface; var T:Terrain; var position : coordonnees; var coord : TSDL_Rect;var fin, u, d, r, l : Boolean;var nbDiamant, Chrono, OldChrono:Integer);
+var event : TSDL_event;
+	portActive : Boolean;
+begin
+	SDL_PollEvent(@event);
+	portActive := False;
+	
+	if nbDiamant > 1 then
+	begin
+		T[19][19].genre := 5;
+		portActive := True
 	end;
 	
+	case event.type_ of
+		SDL_KEYDOWN : 
+			case event.key.keysym.sym of 
+				SDLK_escape : fin := True;
+				SDLK_UP : u := True;
+				SDLK_DOWN : d := True;
+				SDLK_right : r := True;
+				SDLK_left : l := True;
+		end;
+
+		SDL_KEYUP : 
+			case event.key.keysym.sym of 
+				SDLK_UP : u := False;
+				SDLK_DOWN : d := False;
+				SDLK_right : r := False;
+				SDLK_left : l := False;
+		end;
+	end;
+
+	gestionDeplacement(window, T, u, d, r, l, position, coord, nbDiamant);
+
 	if (position.x = 19) and (position.y = 19) and portActive then
 		fin := True;
 	
@@ -571,6 +533,12 @@ begin
 		if random(7)<1 then	
 			moveSpiderAntiClockwise(window, rockford, T, position);
 	end;
+	
+	ecrire(window, IntToStr(OldChrono), 100, 5, 35, 0, 0, 0);
+	SDL_Flip(window);
+	ecrire(window, IntToStr(Chrono), 100, 5, 35, 255, 255, 255);
+	
+	SDL_Flip(window);
 	SDL_Delay(20);
 end;
 
@@ -636,7 +604,7 @@ end;
 
 var window, rockford : PSDL_Surface;
 	coord : TSDL_Rect;
-	niv, nbDiamant : Integer;
+	niv, nbDiamant, Temps, TempsInit, reserveTemps, OldTemps : Integer;
 	fin : Boolean;
 	position : coordonnees;
 	T : Terrain;
@@ -649,12 +617,12 @@ begin
 	position.y := 3;
 	niv := 1; //random(10) + 1;
 	coord.x := 32*(position.x-1);
-	coord.y := 32*(position.y-1);
+	coord.y := 32*(position.y-1) + 50;
 	
 	fin := False;
 	chargement('ressources/Niveaux v1/v1-' + IntToStr(niv),T);
 
-	afficherfond(window, rockford, T, position);
+	afficherfond(window, rockford, T, position, True);
 	SDL_BlitSurface(rockford, NIL, window,@coord);
 	SDl_Flip(window);
 	SDl_Delay(20);
@@ -663,8 +631,13 @@ begin
 	r := False;	l := False;
 	nbDiamant := 0;
 	
+	reserveTemps := 60;
+	TempsInit := SDL_GetTicks() div 1000;
 	repeat
-		deplacementRF(window, rockford, T, position, coord, fin, u, d, r, l, nbDiamant);
+		Temps := reserveTemps - (SDL_GetTicks() - TempsInit) div 1000;
+		deplacementRF(window, rockford, T, position, coord, fin, u, d, r, l, nbDiamant, Temps, OldTemps);
+		OldTemps := Temps;
+		writeln(Temps);		
 	until fin;
 	SDL_FreeSurface(window);
 	SDL_Quit();
